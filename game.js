f8db150cf593aa575a0f871cbfd284355cc803d4
Pantao42 <pantao@kabelmail.de>
2025-03-17 14:03:24 +0100

    Treppe als Bild zugefügt

--- game.js
+++ game.js
@@ -108,6 +108,7 @@ class HenrisSpiel {
         switch(this.currentScene) {
             case 'start':
                 if (input === 'ja') {
+                    this.imageContainer.innerHTML = '<img src="assets/treppenhaus.jpg" alt="Treppenhaus" style="max-width: 100%;">';
                     await this.displayText("Du betrittst die Schule und stehst vor einem Treppenhaus. Gehst du hoch oder runter? (hoch/runter)");
                     this.currentScene = 'treppe';
                 } else if (input === 'nein') {
